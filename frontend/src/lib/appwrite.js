af61719f88bc4387e4fa785513099a71310e9829
TARUN NANDIGAM <tarunnandigam29@gmail.com>
2026-03-25 00:24:07 +0530

    Convert Appwrite hardcoded config to environment variables

--- frontend/src/lib/appwrite.js
+++ frontend/src/lib/appwrite.js
@@ -3,8 +3,8 @@ import { Client, Account } from 'appwrite';
 const client = new Client();
 
 client.
-setEndpoint('https://nyc.cloud.appwrite.io/v1') //API Endpoint
-.setProject('692882a20032126470c2'); //project ID
+setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT) //API Endpoint
+.setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID); //project ID
 
 export const account = new Account(client);
 export { client };
