dbb95b6d6a6d891df34c10d7ab6bfc4e9a281389
TARUN NANDIGAM <tarunnandigam29@gmail.com>
2026-03-26 15:15:29 +0530

    Allow all CORS origins for production deployment

--- backend/src/main/java/com/cloudbox/controller/FileController.java
+++ backend/src/main/java/com/cloudbox/controller/FileController.java
@@ -19,7 +19,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/files")
-@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:5173"})
+@CrossOrigin(origins = "*")
 public class FileController {
     
     @Autowired
